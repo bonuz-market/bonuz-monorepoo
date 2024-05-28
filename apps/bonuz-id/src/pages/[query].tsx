e5b35972094252530c734c3e8c57afe6c0c6ef56
jasonttian <terry.superdev@gmail.com>
2024-05-28 10:29:41 +0300

    update in second page

--- apps/bonuz-id/src/pages/[query].tsx
+++ apps/bonuz-id/src/pages/[query].tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import checkboxIcon from '../../public/icons/checkBox-icon.svg';
 import searchIcon from '../../public/icons/search.svg';
 import cancleIcon from '../../public/icons/arrow-cancle.png';
-
+import staticFace from '../../public/images/static-face.svg';
 
 import Image from 'next/image';
 
@@ -26,40 +26,22 @@ export default function SearchPage() {
     }
 
     return (
-
-
-        <div className="bg-[url('../../public/images/third-baackground.svg')] flex w-full h-full flex-col" >
-            <div className="flex justify-between items-center mt-[28px] flex-col md:flex-row md: gap-2">
+        <div className="bg-[url('/images/third-baackground.svg')] bg-center flex w-full min-h-screen flex-col px-7 pb-6" >
+            <div className="flex justify-between bg-[url('/images/third-baackground.svg')] h-[56px] items-center flex-col rounded-b-[30px] bg-opacity-5 md:flex-row md: gap-2 px-8">
                 <p className='font-[26px] invisible md:visible'>bonuz</p>
-                <button className="w-[30px] h-[30px] bg-[#c5c5c56b] rounded-[50px] flex justify-center items-center">
-                    <svg
-                        className="w-3 h-3 text-white"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 14 8"
-                    >
-                        <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"
-                        />
-                    </svg>
-                </button>
+                <div className="w-[30px] h-[30px] bg-[url('/icons/up-icon.png')] rounded-[50px] bg-center flex justify-center items-center cursor-pointer" onClick={() => router.push('/')} />
                 <button className="rounded-[30px] px-[8px] bg-custom-gradient1">
                     Connect Bonuz On-Chain Social ID
                 </button>
             </div>
 
-            <div className="mt-16 w-full h-[48px] flex justify-between items-center justify-center p-3 pt-0 gap-2 rounded-[30px] border-2 pb-0 pt-0 border-[#9651FF]">
+            <div className="mt-4 w-full h-[48px] flex justify-between items-center justify-center p-3 pt-0 gap-2 rounded-[30px] border-2 pb-0 border-[#9651FF]">
                 <Image src={searchIcon} width={24} height={24} alt="search" />
                 <input name="search" className="w-full bg-transparent font-inter text-base font-normal leading-6 tracking-tight text-left border-none" defaultValue="Search" onChange={(e) => setSearchQuery(e.target.value)} onKeyDown={handleKeyDown} />
                 <Image src={cancleIcon} width={30} height={30} alt="cancle" className="cursor-pointer" />
             </div>
 
-            <div className="flex flex-col mt-4 ">
+            <div className="flex flex-row mt-4 gap-10">
                 <div className="flex flex-col gap-2 ">
                     <div>
                         <p>Digital D/Apps</p>
@@ -105,9 +87,71 @@ export default function SearchPage() {
                                 <p>DEFI</p>
                                 <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
                             </div>
-                            <div className="flex flex-row p-2 border-[1px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
+                            <div className="flex flex-row p-2 border-[1px] max-w-[360px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
                                 <p>Virtual Reality</p>
                                 <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-col gap4'>
+                    <div className='flex flex-col bg-gradient-border mt-10 p-2 rounded-[25px]'>
+                        <p>Humans</p>
+                        <div className='flex flex-row gap-4'>
+                            <div className="rounded-[30px] bg-[url('/images/third-baackground.svg')] p-4 h-[165px] w-[450px] flex flex-row gap-4 justify-center items-center">
+                                <Image src={staticFace} width={107} height={94} alt='static-face' />
+                                <div className='flex flex-col w-full items-center gap-2'>
+                                    <p>Web3 Guru</p>
+                                    <p>@wayneweb3</p>
+                                    <button className="rounded-[30px] px-[8px] h-[35px] bg-custom-gradient1 w-full">
+                                        View Social ID Profile
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="rounded-[30px] bg-[url('/images/third-baackground.svg')] p-4 h-[165px] w-[450px] flex flex-row gap-4 justify-center items-center">
+                                <Image src={staticFace} width={107} height={94} alt='static-face' />
+                                <div className='flex flex-col w-full items-center gap-2'>
+                                    <p>Web3 News</p>
+                                    <p>@web3news</p>
+                                    <button className="rounded-[30px] px-[8px] h-[35px] bg-custom-gradient1 w-full">
+                                        View Social ID Profile
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col bg-gradient-border mt-10 p-2 rounded-[25px]'>
+                        <div className='grid flex-row gap-4 grid-cols-2'>
+                            <div className="rounded-[30px] bg-[url('/images/third-baackground.svg')] p-4 h-[165px] w-[450px] flex flex-row gap-4 justify-center items-center">
+                                <Image src={staticFace} width={107} height={94} alt='static-face' />
+                                <div className='flex flex-col w-full items-center gap-2'>
+                                    <p>Web3 Guru</p>
+                                    <p>@wayneweb3</p>
+                                    <button className="rounded-[30px] px-[8px] h-[35px] bg-custom-gradient1 w-full">
+                                        View Social ID Profile
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="rounded-[30px] bg-[url('/images/third-baackground.svg')] p-4 h-[165px] w-[450px] flex flex-row gap-4 justify-center items-center">
+                                <Image src={staticFace} width={107} height={94} alt='static-face' />
+                                <div className='flex flex-col w-full items-center gap-2'>
+                                    <p>Web3 News</p>
+                                    <p>@web3news</p>
+                                    <button className="rounded-[30px] px-[8px] h-[35px] bg-custom-gradient1 w-full">
+                                        View Social ID Profile
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className="rounded-[30px] bg-[url('/images/third-baackground.svg')] p-4 h-[165px] w-[450px] flex flex-row gap-4 justify-center items-center">
+                                <Image src={staticFace} width={107} height={94} alt='static-face' />
+                                <div className='flex flex-col w-full items-center gap-2'>
+                                    <p>Web3 News</p>
+                                    <p>@web3news</p>
+                                    <button className="rounded-[30px] px-[8px] h-[35px] bg-custom-gradient1 w-full">
+                                        View Social ID Profile
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
--- apps/bonuz-id/tailwind.config.js
+++ apps/bonuz-id/tailwind.config.js
@@ -13,15 +13,15 @@ export const theme = {
       "custom-gradient":
         "linear-gradient(96.3deg, rgba(106, 52, 211, 0.4) 14.82%, rgba(106, 52, 211, 0) 88.09%)",
     },
+    borderImage: {
+      "gradient-border":
+        "1 solid linear-gradient(135.72deg, #57B3F5 0%, rgba(255, 255, 255, 0) 34.5%)",
+    },
     backgroundImage: {
       "custom-gradient":
         "linear-gradient(122.58deg, #e79413 -19.89%, #ea3e5b 48.73%, #fa0af0 119.63%)",
-      "custom-gradient1":
-        "linear-gradient(122.58deg, #e79413 -19.89%, #ea3e5b 48.73%, #fa0af0 119.63%)",
       "custom-bg-gradient":
         "linear-gradient(97.19deg, rgba(106, 52, 211, 0.4) 14.82%, rgba(106, 52, 211, 0) 83.7%)",
-      "custom-border-gradient":
-        "linear-gradient(3.31deg, rgba(255, 161, 16, 0.6) -14.44%, rgba(255, 161, 16, 0) 91.68%)",
     },
   },
 };
@@ -29,11 +29,9 @@ export const plugins = [
   function ({ addUtilities }) {
     addUtilities({
       ".border-custom-gradient": {
-        "border-style": "solid",
-        "border-width": "1px",
         "border-image-source":
-          "linear-gradient(3.31deg, rgba(255, 161, 16, 0.6) -14.44%, rgba(255, 161, 16, 0) 91.68%)",
-        "border-image-slice": 1,
+          "linear-gradient(135.72deg, #57B3F5 0%, rgba(255, 255, 255, 0) 34.5%)",
+        "border-image-slice": "1",
       },
     });
   },
