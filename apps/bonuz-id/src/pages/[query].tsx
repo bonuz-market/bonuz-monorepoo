25fca50ae5cdc70a2a808099f7abbdbca7317318
jasonttian <terry.superdev@gmail.com>
2024-05-29 17:31:26 +0300

    adding count section

--- apps/bonuz-id/src/pages/[query].tsx
+++ apps/bonuz-id/src/pages/[query].tsx
@@ -7,6 +7,21 @@ import staticFace from '../../public/images/static-face.svg';
 
 import Image from 'next/image';
 
+const digitalDappData = [
+    { label: 'ON-CHain Engagement Airdrops', count: '96+' },
+    { label: 'Education Certificates', count: '99+' },
+    { label: 'Shopping (Vouchers, Gift Cards)', count: '99+' },
+    { label: 'DEFI', count: '99+' },
+    { label: 'Virtual Reality', count: '99+' },
+]
+const realWorldData = [
+    { label: 'Humans (Bonuz On-Chain ID)', count: '96+' },
+    { label: 'Lens Profiles', count: '99+' },
+    { label: 'Real Life Engagement Airdrops', count: '99+' },
+    { label: '99+', count: '' },
+    { label: 'Token-Gated Meetup', count: '99+' },
+    { label: 'Mixed Reality Games (AR)', count: '99+' },
+]
 export default function SearchPage() {
     const router = useRouter();
     const { query } = router.query;
@@ -37,7 +52,7 @@ export default function SearchPage() {
 
             <div className="mt-4 w-full h-[48px] flex justify-between items-center justify-center p-3 pt-0 gap-2 rounded-[30px] border-2 pb-0 border-[#9651FF]">
                 <Image src={searchIcon} width={24} height={24} alt="search" />
-                <input name="search" className="w-full bg-transparent font-inter text-base font-normal leading-6 tracking-tight text-left border-none" defaultValue="Search" onChange={(e) => setSearchQuery(e.target.value)} onKeyDown={handleKeyDown} />
+                <input name="search" className="w-full outline-none bg-transparent font-inter text-base font-normal leading-6 tracking-tight text-left border-none" defaultValue="Search" onChange={(e) => setSearchQuery(e.target.value)} onKeyDown={handleKeyDown} />
                 <Image src={cancleIcon} width={30} height={30} alt="cancle" className="cursor-pointer" />
             </div>
 
@@ -46,55 +61,37 @@ export default function SearchPage() {
                     <div>
                         <p>Digital D/Apps</p>
                         <div className="flex flex-col gap-2 pt-6">
-                            <div className="flex flex-row p-2 max-w-[360px] border-[1px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
-                                <p>On-Chain Engagement Airdrops</p>
-                                <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
-                            </div>
-                            <div className="flex flex-row p-2 max-w-[360px] border-[1px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
-                                <p>Education Certificates</p>
-                                <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
-                            </div>
-                            <div className="flex flex-row p-2 max-w-[360px] border-[1px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
-                                <p>Shopping (Vouchers, Gift Cards)</p>
-                                <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
-                            </div>
-                            <div className="flex flex-row p-2 max-w-[360px] border-[1px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
-                                <p>DEFI</p>
-                                <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
-                            </div>
-                            <div className="flex flex-row p-2 max-w-[360px] border-[1px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
-                                <p>Virtual Reality</p>
-                                <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
-                            </div>
+                            {digitalDappData.map((data, index) => (
+                                <div key={index} className="flex flex-row p-2 gap-4 max-w-[360px] border-[1px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
+                                    <div className='flex flex-row gap-2 text-[16px]'>
+                                        <p>{data.label}</p>
+                                        {data.count !== '' && (
+                                            <div className="flex w-[32px] h-[20px] rounded-[50px] bg-[url('/images/third-baackground.svg')] text-center justify-center items-center text-[13px]">
+                                                {data.count}
+                                            </div>
+                                        )}
+                                    </div>
+                                    <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
+                                </div>
+                            ))}
                         </div>
                     </div>
                     <div className="pt-6">
                         <p>Real-World D/Apps</p>
                         <div className="flex flex-col gap-2 pt-6">
-                            <div className="flex flex-row p-2 max-w-[360px] gap-4 border-[1px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
-                                <p>Humans (Bonuz On-Chain ID)</p>
-                                <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
-                            </div>
-                            <div className="flex flex-row p-2 max-w-[360px] border-[1px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
-                                <p>Lens Profiles</p>
-                                <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
-                            </div>
-                            <div className="flex flex-row p-2 max-w-[360px] border-[1px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
-                                <p>Real Life Engagement Airdrops</p>
-                                <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
-                            </div>
-                            <div className="flex flex-row p-2 border-[1px] max-w-[360px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
-                                <p>99+</p>
-                                <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
-                            </div>
-                            <div className="flex flex-row p-2 border-[1px] max-w-[360px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
-                                <p>Token-Gated Meetup</p>
-                                <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
-                            </div>
-                            <div className="flex flex-row p-2 border-[1px] max-w-[360px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
-                                <p>Mixed Reality Games (AR)</p>
-                                <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
-                            </div>
+                            {realWorldData.map((data, index) => (
+                                <div key={index} className="flex flex-row p-2 gap-4 max-w-[360px] border-[1px] rounded-[16px] bg-transparent justify-between font-inter text-base font-normal leading-6 tracking-tight text-left">
+                                    <div className='flex flex-row gap-2 text-[16px]'>
+                                        <p>{data.label}</p>
+                                        {data.count !== '' && (
+                                            <div className="flex w-[32px] h-[20px] rounded-[50px] bg-[url('/images/third-baackground.svg')] text-center justify-center items-center text-[13px]">
+                                                {data.count}
+                                            </div>
+                                        )}
+                                    </div>
+                                    <Image src={checkboxIcon} width={20} height={20} alt="checkbox_icon" />
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
