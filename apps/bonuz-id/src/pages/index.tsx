ac94d5b08f61fe8b78e0749ebbd1cf385f24deb8
jasonttian <terry.superdev@gmail.com>
2024-06-03 18:51:58 +0300

    adjust padding

--- apps/bonuz-id/src/pages/index.tsx
+++ apps/bonuz-id/src/pages/index.tsx
@@ -153,7 +153,7 @@ export default function Home() {
           </div>
         </nav>
 
-        <div className="relative z-[100] pb-[13px] rounded-[40px] w-full flex flex-col px-[5px] py-[5px] md:px-[35px] md:py-[35px] bg-[url('/images/background.png')]">
+        <div className="relative z-[100] pb-[13px] rounded-[40px] w-full flex flex-col px-[5px] py-[5px] md:px-[25px] md:py-[35px] bg-[url('/images/background.png')]">
           <div className="flex w-full flex-col md:flex-row">
             <div className="flex flex-col flex-1">
               <div className="flex justify-between max-w-[700px] p-[10px] gap-0 rounded-[30px] bg-gradient-to-r from-[#0e2875] to-[#4b2ea2] via-[#4b2ea2] flex-col flex-col md:flex-row md:p-[16px]">
