574d08e6a9f419ff15ba02e0c913dc36147e4f40
jasonttian <terry.superdev@gmail.com>
2024-06-08 01:06:56 +0900

    update wallet page

--- apps/bonuz-wallet-expo/providers/ReactQueryProvider.tsx
+++ apps/bonuz-wallet-expo/providers/ReactQueryProvider.tsx
@@ -2,6 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
 import { QueryClient } from '@tanstack/react-query';
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import React from 'react';
 
 const queryClient = new QueryClient({
   defaultOptions: {
