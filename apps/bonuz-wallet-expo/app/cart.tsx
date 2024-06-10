804de3452b0e37b21507c230a232b6bddfb0ef55
jasonttian <terry.superdev@gmail.com>
2024-06-11 03:30:36 +0900

    get data from backend

--- apps/bonuz-wallet-expo/app/cart.tsx
+++ apps/bonuz-wallet-expo/app/cart.tsx
@@ -1,133 +1,103 @@
 /* eslint-disable sonarjs/no-all-duplicated-branches */
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
-import React from 'react';
-import {
-  Image,
-  ImageBackground,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
-import { RFPercentage } from 'react-native-responsive-fontsize';
-import {
-  heightPercentageToDP as hp,
-  widthPercentageToDP as wp,
-} from 'react-native-responsive-screen';
+import React, { useEffect, useState } from 'react';
+import { Image, ImageBackground, ScrollView, StatusBar, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
 import { useShallow } from 'zustand/react/shallow';
 
 import { StatusBarHeight } from '@/components/StatusbarHeight';
-// import SwitchButton from '@/components/SwtichButton';
+import SwitchButton from '@/components/SwtichButton';
 import { Text, View } from '@/components/Themed';
 import { useUserStore } from '@/store';
 import { isNotEmpty } from '@/utils/object';
 
-const walletMockData = {
-  address: '0x71A0...6a6a',
-  balance: '13,941,41',
-};
-
-const defaultWalletData = {
-  address: '0x00...0000',
-  balance: '0',
-};
-
-const coinDatas = [
-  {
-    id: 1,
-    avatar: require('@/assets/images/cart/ethereum.png'),
-    name: 'Ethereum',
-    network: '',
-    tokenAmount: '3.3461',
-    tokenPrice: '~$4,141.25',
-  },
-  {
-    id: 2,
-    avatar: require('@/assets/images/cart/usdc.png'),
-    name: 'USDC',
-    network: 'Polygon',
-    tokenAmount: '3,941.81',
-    tokenPrice: '~$3,941.81',
-  },
-  {
-    id: 3,
-    avatar: require('@/assets/images/cart/bitcoin.png'),
-    name: 'Bitcoin',
-    network: '',
-    tokenAmount: '0.1906',
-    tokenPrice: '~$3,277.53',
-  },
-  {
-    id: 4,
-    avatar: require('@/assets/images/cart/bnb.png'),
-    name: 'BNB',
-    network: '',
-    tokenAmount: '5.0998',
-    tokenPrice: '~$1,423.87',
-  },
-  {
-    id: 5,
-    avatar: require('@/assets/images/cart/solana.png'),
-    name: 'Solana',
-    network: '',
-    tokenAmount: '1,602.98',
-    tokenPrice: '~$1,21.2.81',
-  },
-  {
-    id: 6,
-    avatar: require('@/assets/images/cart/ethereum.png'),
-    name: 'Ethereum',
-    network: '',
-    tokenAmount: '3.3461',
-    tokenPrice: '~$4,141.25',
-  },
-  {
-    id: 7,
-    avatar: require('@/assets/images/cart/usdc.png'),
-    name: 'USDC',
-    network: 'Polygon',
-    tokenAmount: '3,941.81',
-    tokenPrice: '~$3,941.81',
-  },
-  {
-    id: 8,
-    avatar: require('@/assets/images/cart/bitcoin.png'),
-    name: 'Bitcoin',
-    network: '',
-    tokenAmount: '0.1906',
-    tokenPrice: '~$3,277.53',
-  },
-  {
-    id: 9,
-    avatar: require('@/assets/images/cart/bnb.png'),
-    name: 'BNB',
-    network: '',
-    tokenAmount: '5.0998',
-    tokenPrice: '~$1,423.87',
-  },
-  {
-    id: 10,
-    avatar: require('@/assets/images/cart/solana.png'),
-    name: 'Solana',
-    network: '',
-    tokenAmount: '1,602.98',
-    tokenPrice: '~$1,21.2.81',
-  },
-];
+interface TokenData {
+  logoURI: string;
+  symbol: string;
+  balance: number;
+  quote: number;
+}
+interface WalletDataProps {
+  id: number;
+  avatar: any;
+  name: string;
+  network: string;
+  tokenAmount: string;
+  tokenPrice: string;
+}
 
 export default function Cart() {
   const { navigate } = useRouter();
+  const [value, setValue] = useState(false);
+  const [walletData, setWalletData] = useState<WalletDataProps[]>([]);
+  const [tokenData, setTokenData] = useState<TokenData[]>([]);
 
-  const { auth, user } = useUserStore(
+  const [walletAddress, setWalletAddress] = useState<string>('0x00...00000');
+  const [totalBalance, setTotalBalance] = useState<string>('0');
+
+  const { auth, user, wallet } = useUserStore(
     useShallow((store) => ({
       auth: store.auth,
       user: store.user,
+      wallet: store.wallet,
     })),
   );
 
-  // const [isEnabled, setEnabled] = useState(false);
+  useEffect(() => {
+    if (wallet && wallet.address) {
+      setWalletAddress(shortenWalletAddress(wallet.address));
+      if (value === false) {
+        fetch(
+          'https://admin.bonuz.xyz/api/users/wallet/0x0e004bE8F05D53f5E09f61EAAc2acE5314E3438f/balance',
+        ) // Replace with your API URL
+          .then((response) => response.json())
+          .then((data: TokenData[]) => {
+            setTokenData(data.data.tokens);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+      }
+    }
+  }, [value, wallet]);
+
+  useEffect(() => {
+    if (tokenData.length > 0) {
+      let sum = 0,
+        dataArray: React.SetStateAction<WalletDataProps[]> = [];
+      tokenData.map((token, index) => {
+        dataArray.push({
+          id: index + 1,
+          avatar: { uri: token.logoURI },
+          name: token.symbol,
+          network: '',
+          tokenAmount: Number(token.balance).toFixed(4),
+          tokenPrice: '~$' + Number(token.quote).toFixed(2),
+        });
+        sum += Number(token.quote);
+      });
+      setTotalBalance(Number(sum).toFixed(2));
+      setWalletData(dataArray);
+    }
+  }, [tokenData]);
+
+
+  // eslint-disable-next-line unicorn/consistent-function-scoping
+  function shortenWalletAddress(walletAddress: string) {
+    if (!walletAddress || walletAddress.length < 10) {
+      return walletAddress; // If the address is too short, return as is
+    }
+
+    const prefixLength = 5; // Number of characters to keep from the start
+    const suffixLength = 4; // Number of characters to keep from the end
+
+    const prefix = walletAddress.slice(0, prefixLength);
+    const suffix = walletAddress.slice(-suffixLength);
+
+    return `${prefix}...${suffix}`;
+  }
 
   return (
     <LinearGradient colors={['#4B2EA2', '#0E2875']} style={tw`flex-1`}>
@@ -170,39 +140,20 @@ export default function Cart() {
         style={tw`flex-1 mt-[30] bg-transparent`}>
         <ImageBackground
           source={require('@/assets/images/cart/walletBackground.png')}
-          style={tw`h-full w-9/10 rounded-3xl left-2/20 flex-row overflow-hidden`}>
+          style={tw`rounded-3xl flex-row overflow-hidden mx-5`}>
           <View style={tw`w-full bg-transparent p-5 h-full`}>
-            {isNotEmpty(auth) && isNotEmpty(user) ? (
-              <View style={tw`bg-transparent`}>
-                <Text style={tw`font-semibold text-[18px] text-white`}>Main Wallet</Text>
-                <Text style={tw`font-medium text-[14px] text-white`}>{walletMockData.address}</Text>
-                <View style={tw`bg-transparent flex-1 flex-row items-center gap-2`}>
-                  <Text style={tw`text-[20px] text-white font-semibold`}>
-                    ${walletMockData.balance}
-                  </Text>
-                  <Image
-                    style={tw`w-[20.11px] h-[14px]`}
-                    source={require('@/assets/images/cart/eyeIcon.png')}
-                  />
-                </View>
+            <View style={tw`bg-transparent`}>
+              <Text style={tw`font-semibold text-[18px] text-white`}>Main Wallet</Text>
+              <Text style={tw`font-medium text-[14px] text-white`}>{walletAddress}</Text>
+              <View style={tw`bg-transparent flex-1 flex-row items-center gap-2 mt-4`}>
+                <Text style={tw`text-[20px] text-white font-semibold`}>${totalBalance}</Text>
+                <Image
+                  style={tw`w-[20.11px] h-[14px]`}
+                  source={require('@/assets/images/cart/eyeIcon.png')}
+                />
               </View>
-            ) : (
-              <View style={tw`bg-transparent`}>
-                <Text style={tw`font-semibold text-[18px] text-white`}>Main Wallet</Text>
-                <Text style={tw`font-medium text-[14px] text-white mt-1`}>
-                  {defaultWalletData.address}
-                </Text>
-                <View style={tw`bg-transparent flex-1 flex-row items-center gap-2 mt-4`}>
-                  <Text style={tw`text-[20px] text-white font-semibold`}>
-                    ${defaultWalletData.balance}
-                  </Text>
-                  <Image
-                    style={tw`w-[20.11px] h-[14px]`}
-                    source={require('@/assets/images/cart/eyeIcon.png')}
-                  />
-                </View>
-              </View>
-            )}
+            </View>
+
             <View style={tw`flex flex-row bg-transparent justify-between pt-6 px-8`}>
               <View style={tw`bg-transparent items-center text-center gap-2`}>
                 <Image style={tw`w-[54px]`} source={require('@/assets/images/cart/receive.png')} />
@@ -222,32 +173,39 @@ export default function Cart() {
       </ScrollView>
 
       {isNotEmpty(auth) && isNotEmpty(user) ? (
-        <ScrollView style={tw`bg-transparent flex-1`}>
-          {coinDatas.map((coindata, index) => (
-            <View key={index} style={tw`bg-transparent flex-row justify-between p-3`}>
-              <View style={tw`bg-transparent flex flex-row items-center gap-2`}>
-                <Image source={coindata.avatar} />
-                <View style={tw`bg-transparent`}>
-                  <Text style={tw`text-[16px] text-white font-semibold`}>{coindata.name}</Text>
-                  {coindata.network !== '' && (
-                    <Text
-                      style={tw`text-[12px] font-normal text-white bg-[#3953FF] px-2 rounded-2`}>
-                      {coindata.network}
-                    </Text>
-                  )}
+        <View style={tw`flex-1 bg-transparent mt-[-130]`}>
+          <View style={tw`mx-5 bg-transparent mb-5`}>
+            <SwitchButton value={value} onValueChange={setValue} title1="Crypt" title2="NFTs" />
+          </View>
+          <ScrollView style={tw`bg-transparent flex-1`}>
+            {walletData.map((coindata, index) => (
+              <View key={index} style={tw`bg-transparent flex-row justify-between p-3 mx-5`}>
+                <View style={tw`bg-transparent flex flex-row items-center gap-2`}>
+                  <Image style={tw`w-[30px] h-[30px]`} source={coindata.avatar} />
+                  <View style={tw`bg-transparent`}>
+                    <Text style={tw`text-[16px] text-white font-semibold`}>{coindata.name}</Text>
+                    {coindata.network !== '' && (
+                      <Text
+                        style={tw`text-[12px] font-normal text-white bg-[#3953FF] px-2 rounded-2`}>
+                        {coindata.network}
+                      </Text>
+                    )}
+                  </View>
+                </View>
+                <View style={tw`bg-transparent items-end`}>
+                  <Text style={tw`text-white text-[16px] font-semibold`}>
+                    {coindata.tokenAmount}
+                  </Text>
+                  <Text style={tw`text-white text-[14px] font-normal`}>{coindata.tokenPrice}</Text>
                 </View>
               </View>
-              <View style={tw`bg-transparent`}>
-                <Text>{coindata.tokenAmount}</Text>
-                <Text>{coindata.tokenPrice}</Text>
-              </View>
-            </View>
-          ))}
-        </ScrollView>
+            ))}
+          </ScrollView>
+        </View>
       ) : (
         <ScrollView style={tw`mx-5`}>
           <View
-            style={tw`bg-transparent flex flex-row items-center justify-between bg-[#373BA1] p-3 rounded-4`}>
+            style={tw`bg-transparent flex flex-row items-center justify-between bg-[#373BA1] p-3 mx-5 rounded-4`}>
             <View style={tw`bg-transparent flex flex-row gap-4`}>
               <View style={tw`w-[50px] bg-[#2F1385] rounded-2`} />
               <View style={tw`bg-transparent gap-2`}>
--- apps/bonuz-wallet-expo/components/SwtichButton.tsx
+++ apps/bonuz-wallet-expo/components/SwtichButton.tsx
@@ -1,85 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { Animated, Pressable, StyleSheet, View } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
+import React from 'react';
+import { Pressable, Text, View } from 'react-native';
+import tw from 'twrnc';
 
-const SwitchButton = (props: { value: any; onValueChange: any }) => {
-    const defaultStyles = {
-        bgGradientColors: ['#0100ff', '#ff00fb'],
-        headGradientColors: ['#ffffff', '#E1E4E8'],
-    };
-
-    const activeStyles = {
-        bgGradientColors: ['#00c4ff', '#fff600'],
-        headGradientColors: ['#444D56', '#0E1723'],
-    };
-
-    const { value, onValueChange } = props;
-    const [animatedValue] = useState(new Animated.Value(value ? 1 : 0));
-    const [currentStyles, setCurrnetStyles] = useState({});
-
-    useEffect(() => {
-        Animated.timing(animatedValue, {
-            toValue: value ? 1 : 0,
-            duration: 300, // Adjust the animation duration
-            useNativeDriver: false,
-        }).start();
-        if (value === 1) setCurrnetStyles(activeStyles);
-        setCurrnetStyles(defaultStyles);
-    }, [value]);
-
-    const translateX = animatedValue.interpolate({
-        inputRange: [0, 1],
-        outputRange: [4, 28], // Adjust the distance of the switch head
-    });
-
+export default function SwitchButton(props: {
+    value: any;
+    onValueChange: any;
+    title1: string;
+    title2: string;
+}) {
+    const { value, onValueChange, title1, title2 } = props;
     const toggleSwitch = () => {
         const newValue = !value;
+        console.log(newValue);
         onValueChange(newValue);
     };
 
     return (
-        <Pressable onPress={toggleSwitch} style={styles.pressable}>
-            <LinearGradient
-                colors={currentStyles.bgGradientColors}
-                style={styles.backgroundGradient}
-                start={{
-                    x: 0,
-                    y: 0.5,
-                }}>
-                <View style={styles.innerContainer}>
-                    <Animated.View
-                        style={{
-                            transform: [{ translateX }],
-                        }}>
-                        <LinearGradient colors={currentStyles.headGradientColors} style={styles.headGradient} />
-                    </Animated.View>
+        <Pressable onPress={toggleSwitch}>
+            <View style={tw`flex h-[48px] bg-[#413DAB] rounded-[50px] p-1 flex-row`}>
+                <View style={[
+                    tw`w-1/2 h-full rounded-full justify-center items-center`,
+                    value ? tw`bg-transparent` : tw`bg-white`,
+                ]}>
+                    <Text style={[tw`font-semibold text-[16px]`, value ? tw`text-white` : tw`text-black`]}>
+                        {title1}
+                    </Text>
                 </View>
-            </LinearGradient>
+                <View style={[
+                    tw`w-1/2 h-full rounded-full justify-center items-center`,
+                    value ? tw`bg-white` : tw`bg-transparent`,
+                ]}>
+                    <Text style={[tw`font-semibold text-[16px]`, value ? tw`text-black` : tw`text-white`]}>
+                        {title2}
+                    </Text>
+                </View>
+            </View>
         </Pressable>
     );
-};
-
-const styles = StyleSheet.create({
-    pressable: {
-        width: 56,
-        height: 32,
-        borderRadius: 16,
-    },
-    backgroundGradient: {
-        borderRadius: 16,
-        flex: 1,
-    },
-    innerContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        flex: 1,
-        position: 'relative',
-    },
-    headGradient: {
-        width: 24,
-        height: 24,
-        borderRadius: 100,
-    },
-});
-
-export default SwitchButton;
+}
