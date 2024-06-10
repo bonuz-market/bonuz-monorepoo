e85dfd36672f0302700d8bc4846d1e83ca4d8f9e
jasonttian <terry.superdev@gmail.com>
2024-06-11 05:15:54 +0900

    update wallet page 6-10-5

--- apps/bonuz-wallet-expo/components/LoadingSection.tsx
+++ apps/bonuz-wallet-expo/components/LoadingSection.tsx
@@ -3,7 +3,6 @@ import { Text, View } from 'react-native';
 import tw from 'twrnc';
 
 export default function LoadingSection() {
-
     return (
         <View style={tw`bg-transparent flex-row justify-center p-3 mx-5 items-center`}>
             <Text style={tw`text-white text-[30px]`}>Loading...</Text>
