9156d775e2d014b9d25cf1f88355d4db488af7dc
jasonttian <terry.superdev@gmail.com>
2024-06-14 06:33:54 +0900

    add click event for nftinfo

--- apps/bonuz-wallet-expo/components/NftInfo.tsx
+++ apps/bonuz-wallet-expo/components/NftInfo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import tw from 'twrnc';
 
 import LoadingSection from './LoadingSection';
@@ -24,22 +24,23 @@ export default function NftInfoSection(props: { value: any; loadingStatus: boole
                 <>
                     {value.length > 0 ? (
                         value.map((coindata: NftDataProps, index: number) => (
-                            <View
-                                key={index}
-                                style={tw`bg-transparent flex-row justify-between p-3 mx-5 mt-3 mb-3 bg-[#291167] rounded-xl`}>
-                                <View style={tw`bg-transparent flex flex-row items-center gap-2`}>
-                                    <Image style={tw`w-[80px] h-[80px]`} source={coindata.avatar} />
-                                    <View style={tw`bg-transparent w-78/100 flex flex-col gap-2`}>
-                                        <Text style={tw`text-[16px] text-white font-semibold flex-wrap`}>
-                                            {coindata.name}
-                                        </Text>
-                                        <Text style={tw`text-[12px] font-normal text-white`}>
-                                            {coindata.description}
-                                        </Text>
-                                        <Text style={tw`text-[12px] font-normal text-white`}>{coindata.date}</Text>
+                            <TouchableOpacity key={index} onPress={() => console.log('clicked')}>
+                                <View
+                                    style={tw`bg-transparent flex-row justify-between p-3 mx-5 mt-3 mb-3 bg-[#291167] rounded-xl`}>
+                                    <View style={tw`bg-transparent flex flex-row items-center gap-2`}>
+                                        <Image style={tw`w-[80px] h-[80px]`} source={coindata.avatar} />
+                                        <View style={tw`bg-transparent w-78/100 flex flex-col gap-2`}>
+                                            <Text style={tw`text-[16px] text-white font-semibold flex-wrap`}>
+                                                {coindata.name}
+                                            </Text>
+                                            <Text style={tw`text-[12px] font-normal text-white`}>
+                                                {coindata.description}
+                                            </Text>
+                                            <Text style={tw`text-[12px] font-normal text-white`}>{coindata.date}</Text>
+                                        </View>
                                     </View>
                                 </View>
-                            </View>
+                            </TouchableOpacity>
                         ))
                     ) : (
                         <NoItemSection />
