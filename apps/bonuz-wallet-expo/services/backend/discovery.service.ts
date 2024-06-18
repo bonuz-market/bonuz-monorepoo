9d44a7b657a6cb7579a163748000d160aeab7554
Mostafa Abouzeid <mostafatalaat770@gmail.com>
2024-06-18 21:58:16 +0300

    feat(wallet): add home featured items carousel
    Signed-off-by: Mostafa Abouzeid <mostafatalaat770@gmail.com>

---
+++ apps/bonuz-wallet-expo/services/backend/discovery.service.ts
@@ -0,0 +1,14 @@
+export const getFeaturedItems = async () => {
+  // TODO: Fetch featured items from the backend.
+  return [
+    {
+      image: require('@/assets/images/home/banner.png'),
+    },
+    {
+      image: require('@/assets/images/home/banner.png'),
+    },
+    {
+      image: require('@/assets/images/home/banner.png'),
+    },
+  ];
+};
