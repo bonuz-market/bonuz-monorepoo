451c7b3028d19ebf7b3201a2e47292d2f0ada3e9
Mostafa Abouzeid <mostafatalaat770@gmail.com>
2024-06-19 20:36:23 +0300

    update: make tabs more customizable
    Signed-off-by: Mostafa Abouzeid <mostafatalaat770@gmail.com>

--- apps/bonuz-wallet-expo/components/Tabs.tsx
+++ apps/bonuz-wallet-expo/components/Tabs.tsx
@@ -6,29 +6,46 @@ export const Tabs = <T extends string>({
   value,
   onValueChange,
   tabs,
+  style,
+  activeStyle,
+  textStyle,
+  activeTextStyle,
+  containerStyle,
 }: {
   value: T;
   onValueChange: (value: T) => void;
   tabs: T[];
+  style?: any;
+  activeStyle?: any;
+  textStyle?: any;
+  activeTextStyle?: any;
+  containerStyle?: any;
 }) => {
   const onChange = (title: T) => {
     onValueChange(title);
   };
 
+  const _containerStyle = containerStyle ?? tw` bg-[#413DAB]`;
+  const _style = style ?? tw`bg-transparent`;
+  const _activeStyle = activeStyle ?? tw`bg-white`;
+  const _textStyle = textStyle ?? tw`text-white`;
+  const _activeTextStyle = activeTextStyle ?? tw`text-black`;
+
   return (
-    <View style={tw`flex h-[48px] bg-[#413DAB] rounded-[50px] p-1 flex-row`}>
+    <View style={[tw`flex h-[48px] rounded-full p-1 flex-row gap-2`, _containerStyle]}>
       {tabs.map((title, index) => (
         <Pressable
           key={index}
           onPress={() => onChange(title)}
           style={[
-            tw`flex-1 h-full rounded-full justify-center items-center`,
-            value === title ? tw`bg-white` : tw`bg-transparent`,
+            tw`flex-1 justify-center items-center`,
+            value === title && tw`rounded-full`,
+            value === title ? _activeStyle : _style,
           ]}>
           <Text
             style={[
               tw`font-semibold text-[16px]`,
-              value === title ? tw`text-black` : tw`text-white`,
+              value === title ? _activeTextStyle : _textStyle,
             ]}>
             {title}
           </Text>
