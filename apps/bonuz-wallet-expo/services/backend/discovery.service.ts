681c357e1e76156c8960187a8bc7c81064082a77
Mostafa Abouzeid <mostafatalaat770@gmail.com>
2024-06-20 21:21:39 +0300

    feat: init home integration
    Signed-off-by: Mostafa Abouzeid <mostafatalaat770@gmail.com>

--- apps/bonuz-wallet-expo/services/backend/discovery.service.ts
+++ apps/bonuz-wallet-expo/services/backend/discovery.service.ts
@@ -1,3 +1,7 @@
+import { AppView } from '@/entities/discovery';
+
+import { backendClient } from './backend.config';
+
 export const getFeaturedItems = async () => {
   // TODO: Fetch featured items from the backend.
   return [
@@ -12,3 +16,21 @@ export const getFeaturedItems = async () => {
     },
   ];
 };
+
+export const getRealWorldData = async () =>
+  backendClient
+    .get('api/realworld', {
+      searchParams: {
+        markdown: true,
+      },
+    })
+    .json<AppView['RealWorlds']>();
+
+export const getDigitalWorldData = async () =>
+  backendClient
+    .get('api/digitalworld', {
+      searchParams: {
+        markdown: true,
+      },
+    })
+    .json<AppView['DigitalWorlds']>();
