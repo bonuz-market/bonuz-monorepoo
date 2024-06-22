de0c834f1b83e4228f4f14a26376025b7d5e131a
Mostafa Abouzeid <mostafatalaat770@gmail.com>
2024-06-22 15:05:51 +0300

    fix: category page back button
    Signed-off-by: Mostafa Abouzeid <mostafatalaat770@gmail.com>

--- apps/bonuz-wallet-expo/app/(discover)/digitalWorld/_layout.tsx
+++ apps/bonuz-wallet-expo/app/(discover)/digitalWorld/_layout.tsx
@@ -15,15 +15,19 @@ export default function DiscoverLayout() {
             <LinearGradient colors={['#0E2875', '#4B2EA2']} start={[0, 0]} end={[1, 1]}>
               <View style={tw`rounded-b-[30px] pb-4 px-4  bg-[#553eb4]`}>
                 <View style={tw`flex-row items-center bg-transparent`}>
-                  <View style={tw`h-[48px]`}>
+                  <View style={tw`h-[48px] w-[48px] z-50`}>
                     <Pressable
                       onPress={navigation.goBack}
                       hitSlop={30}
-                      style={tw`p-3 absolute rounded-full bg-[#6742c1] z-50`}>
+                      style={tw`p-3 absolute rounded-full bg-[#6742c1] `}>
                       <Iconify icon="ion:chevron-back-outline" color="white" size={24} />
                     </Pressable>
                   </View>
-                  <Text style={tw`text-white text-center flex-1 text-xl font-semibold`}>
+                  <Text
+                    style={[
+                      tw`text-white text-center flex-1 text-xl font-semibold`,
+                      { transform: [{ translateX: -24 }] },
+                    ]}>
                     {options.title}
                   </Text>
                 </View>
