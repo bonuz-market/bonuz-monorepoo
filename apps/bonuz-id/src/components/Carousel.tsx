b3deaf93b0f6b31bffcc1127a7545d8ba6ad0854
jasonttian <terry.superdev@gmail.com>
2024-06-26 00:33:27 +0900

    update carousel in profile

--- apps/bonuz-id/src/components/Carousel.tsx
+++ apps/bonuz-id/src/components/Carousel.tsx
@@ -23,6 +23,21 @@ export default function Carousel(props: ICarouselProps): React.ReactElement {
         },
     };
 
+    const getGradientClass = (index: number) => {
+        switch (index % 4) {
+            case 0:
+                return "bg-gradient-to-r from-[#F67640] to-[#F14375]";
+            case 1:
+                return "bg-gradient-to-r from-[#009EFD] to-[#2AF598]";
+            case 2:
+                return "bg-gradient-to-r from-[#375F90] to-[#7335FF]";
+            case 3:
+                return "bg-gradient-to-r from-[#FFA34E] to-[#FFA110]";
+            default:
+                return "";
+        }
+    };
+
     return (
         <div className="relative overflow-hidden w-full">
             <Swiper
@@ -35,7 +50,8 @@ export default function Carousel(props: ICarouselProps): React.ReactElement {
             >
                 {(props.slides || []).map((slide, index) => (
                     <SwiperSlide key={index} className={cn(
-                        "flex flex-row p-4 gap-2 rounded-[20px] bg-gradient-to-r from-[#009EFD] to-[#2AF598] shadow-[0px_0px_0px_0px_rgba(0,0,0,0.00),-80px_104px_53px_0px_rgba(0,0,0,0.01),-45px_59px_44px_0px_rgba(0,0,0,0.05),-20px_26px_33px_0px_rgba(0,0,0,0.09),-5px_7px_18px_0px_rgba(0,0,0,0.10)] backdrop-blur-[20px]",
+                        "flex flex-row p-4 gap-2 rounded-[20px] shadow-[0px_0px_0px_0px_rgba(0,0,0,0.00),-80px_104px_53px_0px_rgba(0,0,0,0.01),-45px_59px_44px_0px_rgba(0,0,0,0.05),-20px_26px_33px_0px_rgba(0,0,0,0.09),-5px_7px_18px_0px_rgba(0,0,0,0.10)] backdrop-blur-[20px]",
+                        getGradientClass(index)
                     )}>
                         <div className="flex flex-col w-full">
                             <div className="flex flex-col w-[60px] h-[60px] justify-center items-start p-4 bg-blend-overlay rounded-[70px]">
